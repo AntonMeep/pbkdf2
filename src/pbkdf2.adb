1fa55e3d4897bf8e78780609cbb41627b8fdadf3
Anton Fediushin <imantonmeep@gmail.com>
2022-03-13 19:22:01 +0100

    Update, make it more pretty

--- src/pbkdf2.adb
+++ src/pbkdf2.adb
@@ -3,66 +3,128 @@ pragma Ada_2012;
 with Interfaces;
 
 package body PBKDF2 is
-  function Write_Big_Endian(Input : Stream_Element_Offset) return Stream_Element_Array is
-    use Interfaces;
+   function Write_Big_Endian
+     (Input : Stream_Element_Offset) return Stream_Element_Array
+   is
+      use Interfaces;
 
-    Int : constant Unsigned_32 := Unsigned_32(Input);
-  begin
-    return (
-      0 => Stream_Element(Shift_Right(Int, 24) and 16#FF#),
-      1 => Stream_Element(Shift_Right(Int, 16) and 16#FF#),
-      2 => Stream_Element(Shift_Right(Int, 8) and 16#FF#),
-      3 => Stream_Element(Int and 16#FF#)
-    );
-  end Write_Big_Endian;
+      Int : constant Unsigned_32 := Unsigned_32 (Input);
+   begin
+      return
+        (0 => Stream_Element (Shift_Right (Int, 24) and 16#FF#),
+         1 => Stream_Element (Shift_Right (Int, 16) and 16#FF#),
+         2 => Stream_Element (Shift_Right (Int, 8) and 16#FF#),
+         3 => Stream_Element (Int and 16#FF#));
+   end Write_Big_Endian;
 
-  function F(Password       : String; Salt : String; Iterations : Natural;
-      I : Stream_Element_Offset) return Stream_Element_Array is
-      
-      use GNAT.SHA1;
-      Result : Binary_Message_Digest;
-      Last : Binary_Message_Digest;
-      Ctx : Context;
-    begin
-      --  First iteration
-      Ctx := HMAC_Initial_Context(Password);
-      Update(Ctx, Salt);
-      Update(Ctx, Write_Big_Endian(I));
-      Result := Digest(Ctx);
-      Last := Result;
+   procedure XOR_In_Place
+     (L : in out Stream_Element_Array; R : Stream_Element_Array)
+   is
+   begin
+      pragma Assert (L'Length = R'Length);
 
-      --  Subsequent iterations
-      for J in 2..Iterations loop
-        Ctx := HMAC_Initial_Context(Password);
-        Update(Ctx, Last);
-        Last := Digest(Ctx);
-        for L in Result'Range loop
-          Result(L) := Result(L) xor Last(L);
-        end loop;
+      for I in L'Range loop
+         L (I) := L (I) xor R (I);
       end loop;
+   end XOR_In_Place;
+
+   generic
+      type Context (<>) is private;
+      with function HMAC_Init (Password : String) return Context;
+      with procedure HMAC_Update_Salt (Ctx : in out Context; Salt : String);
+      with procedure HMAC_Update_Data
+        (Ctx : in out Context; Data : Stream_Element_Array);
+      with function HMAC_Final (Ctx : Context) return Stream_Element_Array;
+
+      Hash_Length : Stream_Element_Offset;
+   function PBKDF2_Implementation
+     (Password           : String; Salt : String; Iterations : Positive;
+      Derived_Key_Length : Stream_Element_Offset) return Stream_Element_Array;
+
+   function PBKDF2_Implementation
+     (Password           : String; Salt : String; Iterations : Positive;
+      Derived_Key_Length : Stream_Element_Offset) return Stream_Element_Array
+   is
+      Result  : Stream_Element_Array (1 .. Derived_Key_Length);
+      Current : Stream_Element_Offset := Result'First;
+
+      Blocks_Needed : constant Stream_Element_Offset :=
+        Stream_Element_Offset
+          (Float'Ceiling (Float (Derived_Key_Length) / Float (Hash_Length)));
+   begin
+      for Index in 1 .. Blocks_Needed loop
+         declare
+            Ctx : Context := HMAC_Init (Password);
+
+            Temporary, Last : Stream_Element_Array (1 .. Hash_Length);
+         begin
+            --  First iteration
+            HMAC_Update_Salt (Ctx, Salt);
+            HMAC_Update_Data (Ctx, Write_Big_Endian (Index));
+            Temporary := HMAC_Final (Ctx);
+            Last      := Temporary;
+
+            --  Subsequent iterations
+            for Unused in 2 .. Iterations loop
+               Ctx := HMAC_Init (Password);
+               HMAC_Update_Data (Ctx, Last);
+
+               Last := HMAC_Final (Ctx);
+               XOR_In_Place (Temporary, Last);
+            end loop;
+
+            Result (Current .. Current + Hash_Length) := Temporary;
+            Current                                   := Current + Hash_Length;
+         end;
+      end loop;
+
       return Result;
-    end F;
+   end PBKDF2_Implementation;
 
    function PBKDF2_HMAC_SHA1
-     (Password       : String; Salt : String; Iterations : Natural;
-      Derived_Length : Stream_Element_Offset := GNAT.SHA1.Hash_Length)
+     (Password           : String; Salt : String; Iterations : Positive;
+      Derived_Key_Length : Stream_Element_Offset := GNAT.SHA1.Hash_Length)
       return Stream_Element_Array
    is
-    use GNAT.SHA1;
-
-    Result : Stream_Element_Array(1 .. Derived_Length) := (others => 0);
-    Current : Stream_Element_Offset := Result'First;
-
-    Blocks_Needed : Stream_Element_Offset :=
-      Stream_Element_Offset(Float'Ceiling(Float(Derived_Length) / Float(Hash_Length)));
-    
+      function Implementation is new PBKDF2_Implementation
+        (Context          => GNAT.SHA1.Context,
+         HMAC_Init        => GNAT.SHA1.HMAC_Initial_Context,
+         HMAC_Update_Salt => GNAT.SHA1.Update,
+         HMAC_Update_Data => GNAT.SHA1.Update, HMAC_Final => GNAT.SHA1.Digest,
+         Hash_Length      => GNAT.SHA1.Hash_Length);
    begin
-      for I in 1..Blocks_Needed loop
-        Result(Current .. Current + Hash_Length) := F(Password, Salt, Iterations, I);
-        Current := Current + Hash_Length;
-      end loop;
-      
-      return Result;
+      return Implementation (Password, Salt, Iterations, Derived_Key_Length);
    end PBKDF2_HMAC_SHA1;
 
+   function PBKDF2_HMAC_SHA256
+     (Password           : String; Salt : String; Iterations : Positive;
+      Derived_Key_Length : Stream_Element_Offset := GNAT.SHA256.Hash_Length)
+      return Stream_Element_Array
+   is
+      function Implementation is new PBKDF2_Implementation
+        (Context          => GNAT.SHA256.Context,
+         HMAC_Init        => GNAT.SHA256.HMAC_Initial_Context,
+         HMAC_Update_Salt => GNAT.SHA256.Update,
+         HMAC_Update_Data => GNAT.SHA256.Update,
+         HMAC_Final       => GNAT.SHA256.Digest,
+         Hash_Length      => GNAT.SHA256.Hash_Length);
+   begin
+      return Implementation (Password, Salt, Iterations, Derived_Key_Length);
+   end PBKDF2_HMAC_SHA256;
+
+   function PBKDF2_HMAC_SHA512
+     (Password           : String; Salt : String; Iterations : Positive;
+      Derived_Key_Length : Stream_Element_Offset := GNAT.SHA512.Hash_Length)
+      return Stream_Element_Array
+   is
+      function Implementation is new PBKDF2_Implementation
+        (Context          => GNAT.SHA512.Context,
+         HMAC_Init        => GNAT.SHA512.HMAC_Initial_Context,
+         HMAC_Update_Salt => GNAT.SHA512.Update,
+         HMAC_Update_Data => GNAT.SHA512.Update,
+         HMAC_Final       => GNAT.SHA512.Digest,
+         Hash_Length      => GNAT.SHA512.Hash_Length);
+   begin
+      return Implementation (Password, Salt, Iterations, Derived_Key_Length);
+   end PBKDF2_HMAC_SHA512;
 end PBKDF2;
--- src/pbkdf2.ads
+++ src/pbkdf2.ads
@@ -1,8 +1,20 @@
 with Ada.Streams; use Ada.Streams;
 
 with GNAT.SHA1;
+with GNAT.SHA256;
+with GNAT.SHA512;
 
 package PBKDF2 is
-    function PBKDF2_HMAC_SHA1 (Password : String; Salt : String; Iterations : Natural; Derived_Length : Stream_Element_Offset := GNAT.SHA1.Hash_Length)
-        return Stream_Element_Array;
+   function PBKDF2_HMAC_SHA1
+     (Password           : String; Salt : String; Iterations : Positive;
+      Derived_Key_Length : Stream_Element_Offset := GNAT.SHA1.Hash_Length)
+      return Stream_Element_Array;
+   function PBKDF2_HMAC_SHA256
+     (Password           : String; Salt : String; Iterations : Positive;
+      Derived_Key_Length : Stream_Element_Offset := GNAT.SHA256.Hash_Length)
+      return Stream_Element_Array;
+   function PBKDF2_HMAC_SHA512
+     (Password           : String; Salt : String; Iterations : Positive;
+      Derived_Key_Length : Stream_Element_Offset := GNAT.SHA512.Hash_Length)
+      return Stream_Element_Array;
 end PBKDF2;
